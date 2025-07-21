f304096e9a861cc7d5f3536ba51968a0853c2f0a
Jeevan-1999 <jeevan@thegamificationcompany.com>
2025-07-21 12:54:03 +0530

    design and development on interview-setup page

--- src/app/dashboard/interview-setup/interview-setup.component.ts
+++ src/app/dashboard/interview-setup/interview-setup.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-interview-setup',
   templateUrl: './interview-setup.component.html',
   styleUrls: ['./interview-setup.component.css']
 })
-export class InterviewSetupComponent {
+export class InterviewSetupComponent implements OnInit {
+  interviewForm!: FormGroup;
+  roundTypes = ['Telephonic', 'Technical', 'HR', 'Group Discussion'];
+  selectedType = '';
+  typeDropdownOpen = false;
+  step = 1;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.interviewForm = this.fb.group({
+      roundName: ['', Validators.required],
+      roundType: ['', Validators.required]
+    });
+  }
+
+  toggleTypeDropdown() {
+    this.typeDropdownOpen = !this.typeDropdownOpen;
+  }
+
+  selectType(type: string) {
+    this.selectedType = type;
+    this.interviewForm.get('roundType')?.setValue(type);
+    this.typeDropdownOpen = false;
+  }
+
+  addRound() {
+    if (this.interviewForm.valid) {
+      const roundData = this.interviewForm.value;
+      console.log('Added Round:', roundData);
+      this.interviewForm.reset();
+      this.selectedType = '';
+    } else {
+      this.interviewForm.markAllAsTouched();
+    }
+  }
+
+  goToSortStep() {
+    if (this.interviewForm.valid) {
+      this.step = 2;
+    } else {
+      this.interviewForm.markAllAsTouched();
+    }
+  }
+
+  onBackClick() {
+    this.router.navigate(['/dashboard']);
+  }
 }
